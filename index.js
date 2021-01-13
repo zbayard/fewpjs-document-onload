08728463a50069cbf783e379b25bc8a100d8a686
Zach <zbayard@gmail.com>
2021-01-12 22:23:57 -0500

    commit tree d5b02f57c9e013b2110315406f9300d34e986a8e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const text = document.querySelector('#text')
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+        text.textContent = "This is really cool!"
+})
